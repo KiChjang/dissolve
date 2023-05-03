7119670af3b6fd79f7498976b1a0964c69ca7b04
Constantin Nickel <constantin.nickel@gmail.com>
2023-05-03 12:59:02 +0200

    Return stripped input as `String`

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,8 @@ use html5ever::tendril::TendrilSink;
 use html5ever::{parse_document, ParseOpts};
 use markup5ever_rcdom::{Node, NodeData, RcDom};
 
-/// Consumes a string that contains HTML5 tags and outputs a Vec<String>
-/// containing the text content inside the tags in a pre-order manner.
+/// Consumes a string that contains HTML5 tags and outputs a `String` containing the text content
+/// inside the tags.
 ///
 /// Basic usage:
 ///
@@ -11,20 +11,20 @@ use markup5ever_rcdom::{Node, NodeData, RcDom};
 /// # use dissolve::strip_html_tags;
 /// let input = "<html>Hello World!</html>";
 /// let output = strip_html_tags(input);
-/// assert_eq!(output, vec!["Hello World!".to_owned()]);
+/// assert_eq!(output, "Hello World!");
 /// ```
-pub fn strip_html_tags(input: &str) -> Vec<String> {
+pub fn strip_html_tags(input: &str) -> String {
     let dom = parse_document(RcDom::default(), ParseOpts::default()).one(input);
     let doc = dom.document;
-    let mut texts = Vec::new();
+    let mut texts = String::new();
     push_texts(&doc, &mut texts);
     texts
 }
 
 /// Helper function to return text in text nodes in pre-order traversal.
-fn push_texts(element: &Node, texts: &mut Vec<String>) {
+fn push_texts(element: &Node, texts: &mut String) {
     if let NodeData::Text { contents } = &element.data {
-        texts.push((**contents.borrow()).to_owned());
+        texts.push_str(&contents.borrow());
     }
     for child in &*element.children.borrow() {
         push_texts(child, texts);
@@ -38,23 +38,20 @@ mod tests {
     fn test_strip_html_tag() {
         let input = "<html>Hello World!</html>";
         let output = strip_html_tags(input);
-        assert_eq!(output, vec!["Hello World!".to_owned()]);
+        assert_eq!(output, "Hello World!");
     }
 
     #[test]
     fn test_strip_nested_tags() {
         let input = "<html>Hello<div>World!</div></html>";
         let output = strip_html_tags(input);
-        assert_eq!(output, vec!["Hello".to_owned(), "World!".to_owned()]);
+        assert_eq!(output, "HelloWorld!");
     }
 
     #[test]
     fn test_preorder_traversal() {
         let input = "<html>Hel<div>lo</div>World!</html>";
         let output = strip_html_tags(input);
-        assert_eq!(
-            output,
-            vec!["Hel".to_owned(), "lo".to_owned(), "World!".to_owned()]
-        );
+        assert_eq!(output, "HelloWorld!");
     }
 }
